0ad77729fbce4218d048fdb0a3eafbd003fc649d
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-01-13 14:16:19 +0600

    shadcn initialization done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import { Button } from "./components/ui/button";
 
 const App = () => {
   return (
     <div>
-      
+      <h1>My Todo</h1>
+      <Button>Add Me</Button>
     </div>
   );
 };
