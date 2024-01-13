c2f9f689933254127a4d89877f36b56ede6e1584
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-01-13 16:07:11 +0600

    todo task working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Button } from "./components/ui/button";
+import Todo from "./pages/Todo";
 
 const App = () => {
   return (
     <div>
-      <h1>My Todo</h1>
-      <Button>Add Me</Button>
+      <Todo/>
     </div>
   );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "./features/todoSlice"
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        todos: todoReducer,
+    }
 })
 
 
