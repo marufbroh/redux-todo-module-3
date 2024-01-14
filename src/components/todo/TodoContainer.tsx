aee37d2a341d6764491480aaafde7180850a5bc6
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-01-14 22:21:32 +0600

    Use Query In RTK Query done

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -2,9 +2,19 @@ import { useAppSelector } from "@/redux/hook";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
+// import { useGetTodosQuery } from "@/redux/api/api";
 
 const TodoContainer = () => {
+  //* From Local State
   const { todos } = useAppSelector((state) => state.todos);
+
+  //* From Server
+  // const { data: todos, isError, isLoading } = useGetTodosQuery(undefined);
+
+  // if(isLoading){
+  //   return <p>Loading...</p>
+  // }
+
   return (
     <div>
       <div className="flex justify-between mb-5">
@@ -14,9 +24,15 @@ const TodoContainer = () => {
 
       <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px]">
         <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
-          {todos.map((todo) => (
+          {/* from local */}
+          {todos?.map((todo) => (
             <TodoCard key={todo.id} todo={todo} />
           ))}
+
+          {/* from server */}
+          {/* {todos?.data?.map((todo) => (
+            <TodoCard key={todo.id} todo={todo} />
+          ))} */}
         </div>
       </div>
     </div>
