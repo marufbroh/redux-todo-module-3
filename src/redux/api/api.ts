6a095bb3dd629d13162eb472265056df6f0d7c61
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-01-15 23:52:56 +0600

    Redux Cache Behaviors

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -4,22 +4,25 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
     reducerPath: "baseApi",
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+    tagTypes: ["todo"],
     endpoints: (builder) => ({
         getTodos: builder.query({
             query: () => ({
                 url: "/tasks",
                 method: "GET",
-            })
+            }),
+            providesTags: ["todo"]
         }),
         addTodo: builder.mutation({
             query: (data) => ({
                 url: "/tasks",
                 method: "POST",
                 body: data
-            })
+            }),
+            invalidatesTags: ["todo"]
         }),
     }),
 });
 
 
-export const { useGetTodosQuery, useAddTodoMutation  } = baseApi;
+export const { useGetTodosQuery, useAddTodoMutation } = baseApi;
